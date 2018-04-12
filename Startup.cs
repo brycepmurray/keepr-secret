ff7953b0754a7dbd8730e37c727fd780602fe25d
Bryce Murray <brycepmurray711@gmail.com>
2018-04-12 16:43:41 -0600

    added comment for later?

--- Startup.cs
+++ Startup.cs
@@ -51,6 +51,7 @@ namespace keepr_secret
             });
 
             services.AddMvc();
+            //add Repositories here?
         }
         
         private IDbConnection CreateDbContext()
