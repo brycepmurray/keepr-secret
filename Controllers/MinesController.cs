be552cd86f6d76e00e7074d0314d7707d485c7d5
Bryce Murray <brycepmurray711@gmail.com>
2018-04-12 21:04:01 -0600

    changed names to match and started Mine Controller

---
+++ Controllers/MinesController.cs
@@ -0,0 +1,18 @@
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Claims;
+using System.Threading.Tasks;
+using keepr_secret.Models;
+using keepr_secret.Repositories;
+using Microsoft.AspNetCore.Authentication;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+
+namespace keepr_secret.Controllers
+{
+    [Route("api/[controller]")]
+    public class MinesController : Controller
+    {
+        private readonly MineRepository db;
+    }
+}
