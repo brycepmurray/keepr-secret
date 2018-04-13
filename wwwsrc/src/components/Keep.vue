d7b50e1da8d854f912abd4bd154765a89047b2fd
Bryce Murray <brycepmurray711@gmail.com>
2018-04-13 02:15:59 -0600

    layout for each vue

---
+++ wwwsrc/src/components/Keep.vue
@@ -0,0 +1,12 @@
+<template>
+<div class="keep">
+    
+</div>
+</template>
+
+<script>
+</script>
+
+<style>
+
+</style>
---
+++ wwwsrc/src/components/Vault.vue
@@ -0,0 +1,12 @@
+<template>
+    <div class="vault">
+
+    </div>
+</template>
+
+<script>
+</script>
+
+<style>
+
+</style>
