7144bceb3a02cd86fdb7885d6a0bc3d128887d37
Bryce Murray <brycepmurray711@gmail.com>
2018-04-15 15:03:30 -0600

    issues with paths for keeps?

--- Controllers/KeepsController.cs
+++ Controllers/KeepsController.cs
@@ -1,4 +1,13 @@
+using Microsoft.AspNetCore.Authentication;
+using Microsoft.AspNetCore.Authorization;
+using System.Collections.Generic;
+using keepr_secret.Repositories;
 using Microsoft.AspNetCore.Mvc;
+using System.Security.Claims;
+using System.Threading.Tasks;
+using keepr_secret.Models;
+using System.Linq;
+
 
 namespace keepr_secret.Controllers
 {
@@ -6,9 +15,28 @@ namespace keepr_secret.Controllers
     public class KeepsController : Controller
     {
         private readonly KeepRepository db;
-        public KeepsController(KeepsController Repository repo)
+        public KeepsController(KeepsController repo)
         {
             db = repo;
         }
+        //GET ALL KEEPS
+        [HttpGet]
+        public IEnumerable<KeepRepository> Get()
+        {
+            return db.GetAllKeeps();
+        }
+
+        //CREATE KEEP
+         [Authorize]
+        [HttpPost]
+        public Keep Post([FromBody]Keep keep)
+        {
+            var user = HttpContext.User.Identity.Name;
+            int Id;
+            int.TryParse( user, out Id);
+            keep.UserId = Id;
+            return db.CreateKeep(keep);
+        }
+
     }
 }
--- Repositories/KeepRepository.cs
+++ Repositories/KeepRepository.cs
@@ -1,4 +1,8 @@
+using System;
+using System.Collections.Generic;
 using System.Data;
+using Dapper;
+using keepr_secret.Models;
 
 namespace keepr_secret.Repositories
 {
@@ -8,6 +12,74 @@ namespace keepr_secret.Repositories
         public KeepRepository(IDbConnection db)
         {
             _db = db;
+        }
+
+        //Create Keep
+        public KeepRepository CreateKeep(Keep keep)
+        {
+            int id = _db.ExecuteScalar<int>($@"INSERT INTO keeps (
+                Name, 
+                ImageUrl,
+                Description, 
+                UserId, 
+                Views, 
+                Saves)"
+                + $@" VALUES(
+                    @Name, 
+                    @ImageUrl, 
+                    @Description, 
+                    @UserId, 
+                    @Views, 
+                    @Saves); 
+                    SELECT LAST_INSERT_ID()", new
+                {
+                    keep.Name,
+                    keep.ImageUrl,
+                    keep.Description,
+                    keep.UserId,
+                    keep.Views,
+                    keep.Saves
+                });
+            keep.Id = id;
+            return keep;
+        }
+
+        //GET ONE BY ID AND UPDATE
+        public Keep UpdateKeep(int id, Keep keep)
+        {
+            return _db.QueryFirstOrDefault<Keep>($@"
+            UPDATE keeps SET                   
+            Name = @Name,
+            Description = @Description,
+            Views = @Views,
+            Saves = @Saves,
+            UserId = @UserId
+            WHERE Id = {id};
+            SELECT * FROM keeps WHERE id = {id};", keep
+            );
+        }
+
+        //DELETE KEEP
+        public string DeleteKeep(int id)
+        {
+            var success = _db.Execute($@"
+            DELETE FROM keeps WHERE Id = {id}", id
+            );
+            return success > 0 ? "success" : "umm that didnt work";
+        }
+
+        //GET ALL KEEPS
+        public IEnumerable<Keep> GetAllKeeps()
+        {
+            return _db.Query<Keep>("SELECT * FROM keeps");
+        }
+
+        //GET BY USER ID
+        public IEnumerable<Keep> GetKeepsByUser(int userId)
+        {
+            return _db.Query<Keep>($@"
+            SELECT * FROM keeps WHERE UserId = {userId}
+            ");
         }
     }
 }
