17c4a0bb39819a2b62ea0353c020da7e02f82036
Bryce Murray <brycepmurray711@gmail.com>
2018-04-16 02:46:34 -0600

    create vault form

--- wwwsrc/src/components/Create.vue
+++ wwwsrc/src/components/Create.vue
@@ -11,6 +11,18 @@
                             <a @click="toggleForm" v-if="createForm">Create Keep</a>
                             <a @click="toggleForm" v-else>Create Vault</a>
                     </div>
+                    <div class="modal-body">
+                        <div v-if="createForm">
+                            <form @submit.prevent="createVault">
+                                <div class="form-group">
+                                    <label for="vault">Vault</label>
+                                    <input type="text" placeholder="name" v-model="vault.name">
+                                    <input type="text" placeholder="description" v-model="vault.description">
+                                    <input type="url" placeholder="imageUrl" v-model="vault.imageUrl">
+                                </div>
+                            </form>
+                        </div>
+                    </div>
                 </div>
             </div>
         </div>
