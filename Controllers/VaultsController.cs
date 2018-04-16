ea4eb0aed071d7f9c792d5b0c1bcab5157280676
Bryce Murray <brycepmurray711@gmail.com>
2018-04-15 18:00:40 -0600

    commented out findvaultbyid, controller seems good

--- Controllers/VaultsController.cs
+++ Controllers/VaultsController.cs
@@ -1,3 +1,4 @@
+using System.Collections.Generic;
 using keepr_secret.Repositories;
 using Microsoft.AspNetCore.Mvc;
 
@@ -10,6 +11,48 @@ namespace keepr_secret.Controllers
         public VaultsController(VaultRepository repo)
         {
             db = repo;
+        }
+        //GET VAULTS BY USER
+        [HttpGet("{userId}")]
+        public IEnumerable<Vault> GetVaultsByUser(int userId)
+        {
+            return db.GetVaultsByUser(userId);
+        }
+
+        //GET VAULT BY ID
+        // [HttpGet("{id}")]
+        // public IEnumerable<Vault> GetVaultById(int id)
+        // {
+        //     return db.GetVaultById(id);
+        // }
+
+        //CREATE VAULT
+        [HttpPost]
+        public Vault CreateVault([FromBody]Vault vault)
+        {
+            var user = HttpContext.User.Identity.Name;
+            int Id;
+            int.TryParse(user, out Id);
+            vault.UserId = Id;
+            return db.CreateVault(vault);
+        }
+
+        //UPDATE VAULT 
+        [HttpPut("{id}")]
+        public Vault Put(int id, [FromBody]Vault vault)
+        {
+            if (ModelState.IsValid)
+            {
+                return db.UpdateVault(id, vault);
+            }
+            return null;
+        }
+
+        //DELETE VAULT
+        [HttpDelete("{id}")]
+            public string Delete(int id)
+        {
+            return db.DeleteVault(id);
         }
     }
 }
