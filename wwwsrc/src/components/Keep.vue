1f52b14cb12eed9f6809782ead7295a1c0f168dd
Bryce Murray <brycepmurray711@gmail.com>
2018-04-16 01:38:52 -0600

    panel header

--- wwwsrc/src/components/Keep.vue
+++ wwwsrc/src/components/Keep.vue
@@ -1,6 +1,20 @@
 <template>
 <div class="keep">
-    
+    <div class="row">
+        <div v-for="item in keeps">
+            <div class="col-sm-3">
+                <div class="panel panel-info">
+                    <div class="panelheading">
+                        <div class="pull-right" v-if="item.userId == user.id">
+                                <button @click="deleteKeep(item.id)">
+                                        <i class="fa fa-trash"></i>
+                                    </button>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
 </div>
 </template>
 
